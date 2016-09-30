bb10090ab09654dd8d094e180c691fca832fc6a9
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-09-30 17:20:34 +0800

    bit more

--- src/myriagon/__main__.py
+++ src/myriagon/__main__.py
@@ -10,7 +10,7 @@ if platform.isMacOSX():
     install()
 elif platform.isLinux():
     DISPLAY_FONT = "Ubuntu Light"
-    FONT_RATIO = 0.8
+    FONT_RATIO = 0.9
     from twisted.internet.gireactor import install
     install(True)
 
