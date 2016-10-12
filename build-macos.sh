5bdaf61a59567e149a134be3d771c6847f2c00a8
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-12 21:47:28 +1100

    os x build script

---
+++ build-macos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf macOS/
+python3 setup.py macos
+python3 -m pip install -U ../toga-cocoa/ -t macOS/Myriagon.app/Contents/Resources/app_packages/
+python3 -m pip install -U ../toga-core/ -t macOS/Myriagon.app/Contents/Resources/app_packages/
+python3 -m pip install -U pyobjc-core -t macOS/Myriagon.app/Contents/Resources/app_packages/
+python3 -m pip install -U pyobjc-framework-CFNetwork -t macOS/Myriagon.app/Contents/Resources/app_packages/
+find macOS/Myriagon.app -iname \*.* | xargs rename -v "s/.cpython-35m-darwin//g"
+gtar -C macOS -caf Myriagon-macOS-sierra.tar.bz2 Myriagon.app
