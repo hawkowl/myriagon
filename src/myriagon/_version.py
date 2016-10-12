14edc2788da21187a67f99c64e2e6e49c116de8f
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-13 10:55:57 +1100

    16.10

--- src/myriagon/_version.py
+++ src/myriagon/_version.py
@@ -1,7 +1,11 @@
+"""
+Provides Myriagon version information.
+"""
+
 # This file is auto-generated! Do not edit!
 # Use `python -m incremental.update Myriagon` to change this file.
 
 from incremental import Version
 
-__version__ = Version('Myriagon', 16, 9, 0)
+__version__ = Version('Myriagon', 16, 10, 0)
 __all__ = ["__version__"]
